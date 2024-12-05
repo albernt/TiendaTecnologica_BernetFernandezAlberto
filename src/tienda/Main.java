17ef340f58c666a09c4bdca4565d9ad4f390ca83
albernt <albertobernet@gmail.com>
2024-12-05 13:01:45 +0100

    Extendemos JFrame en la clase principal, configuramos ventana inicial y llamamos desde main

--- src/tienda/Main.java
+++ src/tienda/Main.java
@@ -1,4 +1,19 @@
 package tienda;
 
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            // Crear y mostrar la ventana
+            JFrame frame = new Tienda();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
 }
--- src/tienda/Tienda.java
+++ src/tienda/Tienda.java
@@ -1,4 +1,21 @@
 package tienda;
 
-public class Tienda {
+import javax.swing.*;
+import java.awt.*;
+
+public class Tienda extends JFrame {
+
+    public Tienda() {
+        setTitle("Mi tienda tecnológica");
+
+        int windowWidth = 1500;
+        int windowHeight = 950;
+
+        // Configuración de la ventana principal
+        setSize(windowWidth, windowHeight);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        getContentPane().setBackground(Color.BLACK);
+    }
 }
